afdcca8f33ade5781180f2f48eee204605510ba3
violetastakic <51881019+violetastakic@users.noreply.github.com>
2019-06-17 18:42:50 +0200

    Add files via upload

---
+++ Aplication/Dto/LoggedUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplication.Dto
+{
+   public class LoggedUserDto
+    {
+       
+     
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public bool IsLogged { get; set; }
+
+    }
+}
---
+++ Aplication/Dto/LoginUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplication.Dto
+{
+    public class LoginUserDto
+    { 
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
